1acec6769df1bb7d7544b7ac0d7ca468efeeda21
Apple <Apple@thehulk-pc.lsmsa.edu>
2012-05-22 12:49:56 -0700

    Commiting new class TwitterModel

---
+++ iOS/LSMSA/LSMSA/TwitterModel.m
@@ -0,0 +1,17 @@
+//
+//  TwitterModel.m
+//  LSMSA
+//
+//  Created by Apple on 5/22/12.
+//  Copyright (c) 2012 NewAperio. All rights reserved.
+//
+
+#import "TwitterModel.h"
+
+@implementation TwitterModel
+
+@synthesize TextBody = _text, DateCreated = _date;
+
+
+
+@end
