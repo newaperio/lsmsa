efe54631d5500ed7ad263547ccf08c4ca6c0cbbc
ecordell <cordell.evan@gmail.com>
2012-05-22 10:01:28 -0500

    Adds barebone iPhone app

---
+++ iOS/LSMSA/LSMSA/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LSMSA
+//
+//  Created by Evan Cordell on 5/22/12.
+//  Copyright (c) 2012 NewAperio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOS/LSMSA/LSMSA/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LSMSA
+//
+//  Created by Evan Cordell on 5/22/12.
+//  Copyright (c) 2012 NewAperio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
