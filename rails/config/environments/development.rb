8ba51e696c23c9eb00a4a8d4d5db27215f0372d9
Matthew Fults <matthewfults@student.lsmsa.edu>
2012-05-22 14:47:07 -0500

    Adds development mailer gem

--- rails/config/environments/development.rb
+++ rails/config/environments/development.rb
@@ -15,6 +15,9 @@ Lsmsa::Application.configure do
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = true
+  
+  # Change mail deliver to either :smtp, :sendmail, :file, :test
+  config.action_mailer.delivery_method = :letter_opener
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
