8331d4851c8c656171e137aa82eab43f8591ad6f
Logan Leger <logan@loganleger.com>
2012-05-22 09:46:02 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lsmsa::Application.config.secret_token = 'a2dca827a5e5f5ab0f42c1a57265cfeb349ce9f81cafc7abfa8a2b658c41be5567ecb83c3aa1918aa2b46c69a19a182ea282e278df637390388729fcb5dd49b6'
