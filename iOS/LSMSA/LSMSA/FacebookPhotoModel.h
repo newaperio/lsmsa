df92beb865d8d4ddf9b2b10e5055570623717cca
Everett Moser <foolsboy137@gmail.com>
2012-05-22 08:56:42 -0700

    Creates FacebookPhotoModel under Models, and creates NewsTableController

---
+++ iOS/LSMSA/LSMSA/FacebookPhotoModel.h
@@ -0,0 +1,15 @@
+//
+//  FacebookPhotoModel.h
+//  LSMSA
+//
+//  Created by Apple on 5/22/12.
+//  Copyright (c) 2012 NewAperio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FacebookPhotoModel : NSObject
+
+
+
+@end
