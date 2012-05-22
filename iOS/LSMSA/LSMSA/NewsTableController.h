e108a82ec0dda176fc295f3ee83303da14b92390
Everett Moser <foolsboy137@gmail.com>
2012-05-22 08:23:20 -0700

    Creates NewsTableController for News table tab

---
+++ iOS/LSMSA/LSMSA/NewsTableController.h
@@ -0,0 +1,13 @@
+//
+//  NewsTableController.h
+//  LSMSA
+//
+//  Created by Apple on 5/22/12.
+//  Copyright (c) 2012 NewAperio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewsTableController : UIViewController
+
+@end
