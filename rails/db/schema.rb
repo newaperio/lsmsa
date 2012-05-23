87069843540700b7e1e51ee54f383140151f9e15
Chase Boles <chsboles@gmail.com>
2012-05-23 10:12:19 -0500

    Mergin

--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120523143125) do
+ActiveRecord::Schema.define(:version => 20120523144047) do
 
   create_table "concentrations", :force => true do |t|
     t.string   "name"
@@ -30,6 +30,13 @@ ActiveRecord::Schema.define(:version => 20120523143125) do
     t.datetime "updated_at",       :null => false
     t.string   "address_2"
     t.string   "year"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
 end
