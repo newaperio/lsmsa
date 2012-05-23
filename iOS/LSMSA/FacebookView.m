a2460d1cdd9ba5a8f633dc9d5c0aa34eaea129a7
Hunter Bouillion <bouillionh@hotmail.com>
2012-05-23 08:03:23 -0700

    trying to push

---
+++ iOS/LSMSA/FacebookView.m
@@ -0,0 +1,31 @@
+//
+//  FacebookView.m
+//  LSMSA
+//
+//  Created by Apple on 5/23/12.
+//  Copyright (c) 2012 NewAperio. All rights reserved.
+//
+
+#import "FacebookView.h"
+
+@implementation FacebookView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
