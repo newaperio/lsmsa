5809bed63971503ac4ea1db2e928ee9c8fe62612
Chase Boles <chsboles@gmail.com>
2012-05-23 12:25:02 -0500

    sort by concentraion fix

--- rails/app/controllers/prospies_controller.rb
+++ rails/app/controllers/prospies_controller.rb
@@ -89,19 +89,9 @@ class ProspiesController < ApplicationController
 	end
   end
    
-   helper_method :sort_column, :sort_direction
+
   
   def index
   	@prospies = Prospy.order(sort_column + ' ' + sort_direction)
-  end
-  
-  private
-  
-  def sort_column
- 		Prospy.column_names.include?(params[:sort]) ? params[:sort] : "name"
-  end
-  
-  def sort_direction
-  		%w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
   end
 end
